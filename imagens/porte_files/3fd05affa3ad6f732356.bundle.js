41a2ad21f622e57664f22ab4052045d5a029185a
Pablo Gonzaga <pabloteclados@gmail.com>
2021-07-02 13:56:07 -0300

    :up: melhorias

---
+++ imagens/porte_files/3fd05affa3ad6f732356.bundle.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3fd05affa3ad6f732356.bundle.js.LICENSE.txt */
+(window["app-designer_WP_JSONP"]=window["app-designer_WP_JSONP"]||[]).push([[49],{3204:function(e,a,d){"use strict";d.r(a),a.default=window.changeAssetPath("app-designer","cdafc305b1ef75035a8de221d66aab7c.png","?hash=20617")}}]);
+//# sourceMappingURL=3fd05affa3ad6f732356.bundle.js.map
